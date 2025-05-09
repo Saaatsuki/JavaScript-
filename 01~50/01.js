5432da431cc8099456d017ee46d9814df281e104
Saaatsuki <166793481+Saaatsuki@users.noreply.github.com>
2025-05-09 15:15:15 +0900

    250509

--- 01~50/01.js
+++ 01~50/01.js
@@ -2,7 +2,18 @@ const a = {a:`a`};
 const b = {b:`b`};
 
 // const c = {...a, ...b};
-const c =  Object.assign({},a,b);
+
+
+// const c =  Object.assign({},a,b);
+// // Object.assign は、複数のオブジェクトを1つにまとめるメソッド。
+// // 最初の引数 {} は「空のオブジェクト」で、ここに a と b の内容をコピーする。
+// // a と b のプロパティがマージされて、c に代入される。
+// console.log(c);
+
+// const c = {...a , ...b};
+
+const c = Object.assign({},a,b);
+
 console.log(c);
 
 
---
+++ 01~50/02.js
@@ -0,0 +1,4 @@
+const arry = ['aa','bb','cc','dd','ee','ff','gg'];
+
+const newArry = arry.slice(3,6);
+console.log(newArry);
