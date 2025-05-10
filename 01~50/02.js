971ac841b4392036b55de1bafce16cf0997c78fc
Saaatsuki <166793481+Saaatsuki@users.noreply.github.com>
2025-05-10 15:15:15 +0900

    250510

--- 01~50/02.js
+++ 01~50/02.js
@@ -1,4 +1,12 @@
 const arry = ['aa','bb','cc','dd','ee','ff','gg'];
 
-const newArry = arry.slice(3,6);
+// const newArry = arry.slice(3,6);
+// const newArry = arry.splice(3,3);
+// const newArry = arry.slice(-4,-1);
+const newArry = arry.filter((val,idx)=> idx>=3 && idx<=5);
+// 配列.filter((要素, インデックス) => 条件式);
+
+
+
+
 console.log(newArry);
